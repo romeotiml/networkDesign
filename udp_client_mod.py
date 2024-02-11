f2d9f5c4a5c9013aad800d7d936e870fffbf7580
Romeo Tim-L <149404382+romeotiml@users.noreply.github.com>
2024-02-11 13:12:45 -0500

    Add files via upload
    for phase 2

---
+++ udp_client_mod.py
@@ -0,0 +1,59 @@
+# Romeo Tim-Louangphixai
+# Student ID: 01819835
+# Network Design: Principles, Protocols & Applications
+# Programming Project Phase 2: UDP Client and Server -->: Implement RDT 1.0 over a reliable UDP channel
+
+#This file if for the creation and managment of the UDP Client
+
+#importing socket library
+from socket import *
+
+#Creating function for message to be sent from the UDP client
+#Define parameters
+
+        #******************************************************* Phase 2 Comments will start at a double tab ************************************************
+
+        # Phase 2 UDP Changes:
+        # * Phase 2 UDP Client will need to read the BMP file in chucks
+        # * Send each chunk as a UDP Packet to the server
+        # * RDT 1.0 assumes that every packet sent is received in order. There is no need for a packet to requested to be re-sent
+
+        #***************************************************************************************************************************************************
+
+        # make_packet function definition
+        # function will take the image file and break it down to several fixed packet size
+
+def make_packet(file, packet_size = 1024):
+
+    #Needs to read the file and allocate packet data at a fixed size
+    while True: #this will start the loop, and until keeping going until it is broken
+        data = file.read(packet_size)
+        if not data: #will check if data is empty --> after the entire file has been read file.read(packet_size) should return a empty byte and then break
+            break
+        yield data #works like return but it will call data without ending the function entirely
+
+        #Changing function name to best match UDP file being sent
+
+def send_udp_file(server_name, server_port, file): #fxn name chance to file instead of message
+    client_socket = socket(AF_INET, SOCK_DGRAM)
+
+    #Attempt to open the file in binary mode --> "rb" will be able to set in binary mode
+    with open(file_path, "rb") as file:
+        #Start to Read and Send the file in packets
+        for packet in make_packet(file): #utilizes make_packet function file chunks and yields these chunk one bby one
+            client_socket.sendto(packet, (server_name, server_port)) #for each packet yielded send to the specified server.
+
+        #Close the Socket
+        client_socket.close() #After all the packet is sent, socket must be closed and will free up system resources.
+
+
+# Example usage
+if __name__ == "__main__":
+    server = 'localhost'  # We are using 'hostname' possibly would have to change for future use
+    port = 12000  # We are using 12000 would have to change if different one is used
+    file_path = input('Enter the path of the BMP file you wish to send: ') #instead of making a specified address this will allow the user to send whatever BMP file if multiple need to be sent, but they can just copy and past the path
+    send_udp_file(server,port,file_path)
+
+#Sources
+#[1] Vinod Vokkarane, "Socket Programming 101 in Python," shared on UML blackboard site for course EECE.4820, Online, Jan 16, 2024, unpublished.
+
---
+++ udp_server_mod.py
@@ -0,0 +1,53 @@
+# Romeo Tim-Louangphixai
+# Student ID: 01819835
+# Network Design: Principles, Protocols & Applications
+# Programming Project Phase 2: UDP CLient and Server --> : Implement RDT 1.0 over a reliable UDP channel
+
+#This file if for the creation and managment of the UDP Server
+
+#importing socket library
+from socket import *
+
+#creating server and defining parameters --> I am using a function to create this server in which would be called onto
+#for organization and for possible need to edit future server name and port number easier
+#to start server all you would have to do is call function define the value of server_pport
+
+#******************************************************* Phase 2 Comments will start at a double tab ************************************************
+
+        # Phase 2 UDP Server Changes:
+        # * Phase 2 it will Recieve data packets from the UDP Client
+        # * Wrote these packets to a BMP file to reconstruct the original file
+        # * Remove any logic related to sending responses back to the client as RDT 1.0 operates under  the assumption of a reliable channel
+
+        #*********************************************************************************************
+def start_udp_server(server_port, file_path):       #adding additional parameter file_path
+    server_socket = socket(AF_INET, SOCK_DGRAM)
+    server_socket.bind(('', server_port))
+    print("The UDP is established now the server is ready to receive!\n")
+
+    # We need to create a output.bmp file so that will be able to write the file with the data from client
+    file_path = file_path = directory_path + '\\output.bmp'
+
+        #Needs to open the file for writing in Binary Mode --> "wb" will open a file for writing in binary  mode
+    with open(file_path, "wb") as file: # -> writing to the variable file
+        while True: # start loop
+            packet, client_address = server_socket.recvfrom(1024) #server waits for data to arrive, receives up too 2048 bytes of data from the client
+            #NOTE: in the textbook the use function calls like extract(packet) and deliver_data
+                ## when you receive a packet using the recvfrom() method, the data you receive is essentially the result of the extract() operation.
+                ###. When you write the received packet to a file (file.write(packet)), you are delivering the data to its final destination (the file), so it serves the deliver_data(data) function
+
+            if not packet: #Will check is the recived message is empty this is a sign that there is no more data needed to be sent.
+                break
+            file.write(packet)
+
+    server_socket.close() #close the socket
+
+
+#Example usage
+if __name__ == "__main__":
+    port = 12000
+    directory_path = input('What is the directory path you would like the create and recieve the BMP file being transferred:')
+    start_udp_server(port, directory_path)
+
+#Sources Used:
+# [1] Vinod Vokkarane, "Socket Programming 101 in Python," shared on UML blackboard site for course EECE.4820, Online, Jan 16, 2024, unpublished.
