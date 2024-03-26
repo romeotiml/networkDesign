c4575019122b355c1ee2dafc3bec493a899a489e
Romeo Tim-L <149404382+romeotiml@users.noreply.github.com>
2024-03-26 15:35:23 -0400

    Update udp_client_modP3.py

--- udp_client_modP3.py
+++ udp_client_modP3.py
@@ -5,53 +5,63 @@
 
 # This file is for the creation and management of the UDP Client
 
-# Importing socket library
-from socket import *
+from socket import socket, AF_INET, SOCK_DGRAM, timeout
 import hashlib
+import struct
 
 # Constants
-SEQUENCE_SIZE = 32  # Size of the sequence number in bits
-PACKET_SIZE = 1024  # Packet size in bytes
+SERVER_ADDR = ('localhost', 12000)
+CLIENT_TIMEOUT = 2.0  # Timeout for ACK waiting
+PACKET_SIZE = 1024  # Data payload size in bytes
 
-# Function to create RDT 2.2 packet
-def create_packet(seq_num, data):
-    seq_num_bytes = seq_num.to_bytes(SEQUENCE_SIZE // 8, 'big')
-    checksum = calculate_checksum(seq_num_bytes + data)
-    return seq_num_bytes + checksum + data
+def make_packet(seq_num, data=b''):
+    """Creates a packet with a sequence number and data, using MD5 for checksum."""
+    seq_bytes = struct.pack('I', seq_num)  # Convert sequence number to bytes
+    checksum = calculate_checksum(seq_bytes + data)
+    return checksum + seq_bytes + data
 
-# Function to calculate checksum using MD5 hashing
 def calculate_checksum(data):
-    hash_object = hashlib.md5()
-    hash_object.update(data)
-    checksum = hash_object.digest()
-    return checksum
+    """Calculates checksum using MD5 hashing."""
+    md5 = hashlib.md5()
+    md5.update(data)
+    return md5.digest()
 
-# Function to send UDP file using RDT 2.2 protocol
-def send_rdt_packets(server_name, server_port, file_path):
-    client_socket = socket(AF_INET, SOCK_DGRAM)
-    with open(file_path, "rb") as file:
-        seq_num = 0
-        for packet_data in make_packet(file):
-            rdt_packet = create_packet(seq_num, packet_data)
-            client_socket.sendto(rdt_packet, (server_name, server_port))
-            seq_num = (seq_num + 1) % (2 ** SEQUENCE_SIZE)
+def verify_checksum(packet):
+    """Verifies if the packet's checksum matches its contents, using MD5."""
+    received_checksum = packet[:16]  # MD5 checksum size
+    seq_and_data = packet[16:]
+    expected_checksum = calculate_checksum(seq_and_data)
+    return received_checksum == expected_checksum
 
-        # Send end-of-file marker
-        eof_marker = create_packet(seq_num, b"EOF")
-        client_socket.sendto(eof_marker, (server_name, server_port))
-
-    client_socket.close()
-
-# Function to break file into fixed-sized packets
-def make_packet(file, packet_size=PACKET_SIZE):
-    while True:
-        data = file.read(packet_size)
-        if not data:
-            break
-        yield data
+def send_file(file_path):
+    try:
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.settimeout(CLIENT_TIMEOUT)
+        
+        # Open the file
+        with open(file_path, 'rb') as f:
+            seq_num = 0
+            while True:
+                data = f.read(PACKET_SIZE)
+                if not data:
+                    break  # File has been read completely
+                
+                while True:
+                    packet = make_packet(seq_num, data)
+                    s.sendto(packet, SERVER_ADDR)
+                    try:
+                        # Wait for ACK
+                        ack_packet, _ = s.recvfrom(1024)
+                        if verify_checksum(ack_packet) and ack_packet[16:] == struct.pack('I', seq_num):
+                            print(f"ACK received for seq {seq_num}")
+                            seq_num = (seq_num + 1) % 2  # Toggle sequence number
+                            break  # Move to next packet
+                    except timeout:
+                        print("Timeout, resending packet")
+    finally:
+        s.close()
 
 if __name__ == "__main__":
-    server = 'localhost'
-    port = 12001
     file_path = input('Enter the path of the JPEG file you wish to send: ')
-    send_rdt_packets(server, port, file_path)
+    send_file(file_path)
+
