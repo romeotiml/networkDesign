8ec44614deab4d7724988795fc8edc311a1b5c3f
chadbboud <147215260+chadbboud@users.noreply.github.com>
2024-03-26 22:34:17 -0400

    Update udp_client_modP3.py

--- udp_client_modP3.py
+++ udp_client_modP3.py
@@ -13,12 +13,16 @@ SEQUENCE_SIZE = 32  # Size of the sequence number in bits
 PACKET_SIZE = 1024  # Packet size in bytes
 
 # Function to create RDT 2.2 packet
+
+
 def create_packet(seq_num, data):
     seq_num_bytes = seq_num.to_bytes(SEQUENCE_SIZE // 8, 'big')
     checksum = calculate_checksum(seq_num_bytes + data)
     return seq_num_bytes + checksum + data
 
 # Function to calculate checksum using MD5 hashing
+
+
 def calculate_checksum(data):
     hash_object = hashlib.md5()
     hash_object.update(data)
@@ -26,6 +30,8 @@ def calculate_checksum(data):
     return checksum
 
 # Function to send UDP file using RDT 2.2 protocol
+
+
 def send_rdt_packets(server_name, server_port, file_path):
     client_socket = socket(AF_INET, SOCK_DGRAM)
     with open(file_path, "rb") as file:
@@ -42,6 +48,8 @@ def send_rdt_packets(server_name, server_port, file_path):
     client_socket.close()
 
 # Function to break file into fixed-sized packets
+
+
 def make_packet(file, packet_size=PACKET_SIZE):
     while True:
         data = file.read(packet_size)
@@ -49,8 +57,10 @@ def make_packet(file, packet_size=PACKET_SIZE):
             break
         yield data
 
+
 if __name__ == "__main__":
     server = 'localhost'
     port = 12001
     file_path = input('Enter the path of the JPEG file you wish to send: ')
     send_rdt_packets(server, port, file_path)
+    
